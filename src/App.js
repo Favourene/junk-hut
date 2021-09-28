10d10b25f389bea4b62dd7d3c645875b6d78c805
igbinosa iwinosa <igbinosaiwinosa01@gmail.com>
2021-09-28 22:07:46 +0100

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Nav from './component/Nav';
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return (
+   <div>
+     <Nav />
+     <section className="hero">
+       <div className="hero-wrapper">
+         <h1>GREAT FOOD</h1>
+         <p>FOR GOOD TIME</p>
+         <button>STARTING - $55</button>
+       </div>
+     </section>
+   </div>
+ )
 }
 
-export default App;
+export default App
---
+++ src/component/Nav.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import './Nav.css'
+import logo from '../image/logo-white.png'
+import bars from '../image/bars-solid.svg'
+
+const Nav = () => {
+  const [isActive, setActive] = useState('false')
+  const handleToggle = () => {
+    setActive(!isActive)
+  }
+  return (
+    <div className={isActive ? 'nav' : 'nav collapse'}>
+      <div className='nav-menu'>
+        <div className='logo'>
+          <img className='img1' src={logo} alt='Logo' />
+        </div>
+        <div className='toggle-icon' onClick={handleToggle}>
+          <div className='menu-icon'>
+            <img className='drop-icon' src={bars} alt='' />
+          </div>
+        </div>
+        <div className='nav-list'>
+          <div className='nav-row'>
+            <a className='list active' href='#'>
+              Home
+            </a>
+            <a className='list' href='#'>
+              About
+            </a>
+            <a className='list' href='#'>
+              Dishes
+            </a>
+            <a className='list' href='#'>
+              Menu
+            </a>
+            <a className='list' href='#'>
+              Chefs
+            </a>
+            <a className='list' href='#'>
+              Testimonies
+            </a>
+            <a className='list' href='#'>
+              Contact
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Nav
